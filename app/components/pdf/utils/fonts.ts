af75294e22b6e698ba692a5c9028fab76ae27ca4
jesusmh <eduardoomh@outlook.com>
2026-02-09 00:47:02 -0600

    Update fonts urls from pdfviewer

--- app/components/pdf/utils/fonts.ts
+++ app/components/pdf/utils/fonts.ts
@@ -5,32 +5,33 @@ export function registerMontserratFonts() {
     family: 'Montserrat',
     fonts: [
       {
-        src: 'https://bimaqujlbuunpcprllcv.supabase.co/storage/v1/object/public/ip-insights-support//Montserrat-Regular.ttf',
+        src: '/fonts/Montserrat-Regular.ttf',
         fontWeight: 'normal',
       },
       {
-        src: 'https://bimaqujlbuunpcprllcv.supabase.co/storage/v1/object/public/ip-insights-support//Montserrat-Italic.ttf',
+        src: '/fonts/Montserrat-Italic.ttf',
         fontStyle: 'italic',
       },
       {
-        src: 'https://bimaqujlbuunpcprllcv.supabase.co/storage/v1/object/public/ip-insights-support//Montserrat-Medium.ttf',
+        src: '/fonts/Montserrat-Medium.ttf',
         fontWeight: 500,
       },
       {
-        src: 'https://bimaqujlbuunpcprllcv.supabase.co/storage/v1/object/public/ip-insights-support//Montserrat-SemiBold.ttf',
+        src: '/fonts/Montserrat-SemiBold.ttf',
         fontWeight: 600,
       },
       {
-        src: 'https://bimaqujlbuunpcprllcv.supabase.co/storage/v1/object/public/ip-insights-support//Montserrat-Bold.ttf',
+        src: '/fonts/Montserrat-Bold.ttf',
         fontWeight: 'bold',
       },
       {
-        src: 'https://bimaqujlbuunpcprllcv.supabase.co/storage/v1/object/public/ip-insights-support//Montserrat-ExtraBold.ttf',
+        src: '/fonts/Montserrat-ExtraBold.ttf',
         fontWeight: 800,
       },
       {
-        src: 'https://bimaqujlbuunpcprllcv.supabase.co/storage/v1/object/public/ip-insights-support//Montserrat-Black.ttf',                 
+        src: '/fonts/Montserrat-Black.ttf',                 
         fontWeight: 900,
       },
     ],
-  })}
+  });
+}
